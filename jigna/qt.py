33f32b639c4faf9115a4ab77d9c716e2d8b5ced3
Prashant Agrawal <prashant@enthought.com>
2015-04-22 09:57:59 +0530

    Fix PyQt imports.

--- jigna/qt.py
+++ jigna/qt.py
@@ -1,5 +1,17 @@
 try:
+    # Set PySide compatible APIs
+    import sip
+    sip.setapi('QString', 2)
+    sip.setapi('QVariant', 2)
     from PyQt4 import QtCore, QtGui, QtNetwork, QtWebKit
+
+    from PyQt4.QtCore import pyqtProperty
+    from PyQt4.QtCore import pyqtSignal
+    from PyQt4.QtCore import pyqtSlot
+
+    QtCore.Property = pyqtProperty
+    QtCore.Signal = pyqtSignal
+    QtCore.Slot = pyqtSlot
 
 except ImportError:
     from PySide import QtCore, QtGui, QtNetwork, QtWebKit
